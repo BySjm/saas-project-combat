d960b1ed0f8cf22bf9ab3c79a0a01c4dadbe1257
tangyangguang <tyg326@126.com>
2019-12-08 12:02:35 +0800

    tyg  improve log of user login IP

--- export_parent/export_manager_web/src/main/java/cn/bysjm/controller/log/AspectLog.java
+++ export_parent/export_manager_web/src/main/java/cn/bysjm/controller/log/AspectLog.java
@@ -29,11 +29,12 @@ public class AspectLog {
     @Autowired
     private HttpServletRequest request;
 
-    @Around("execution(* cn.bysjm.controller.*.*.*(..))")
+    /*@Around("execution(* cn.bysjm.controller.*.*.*(..))")
     public Object saveLog(ProceedingJoinPoint pjp) throws Throwable {
 
         SysLog sysLog = new SysLog();
         User user = (User) session.getAttribute("loginUser");
+
         sysLog.setId(UUID.randomUUID().toString());
         sysLog.setUserName(user.getUserName());
         String ip = request.getRemoteAddr();
@@ -44,12 +45,70 @@ public class AspectLog {
         MethodSignature signature = (MethodSignature) pjp.getSignature();
         Method method = signature.getMethod();//从方法签名中获取方法对象
         sysLog.setMethod(method.getName());//操作方法名
+
         if (method.isAnnotationPresent(RequestMapping.class)){
             sysLog.setAction(method.getAnnotation(RequestMapping.class).name());//方法上@RequestMapping注解的name值
         }
         sysLogService.save(sysLog);
         //执行原方法
         return pjp.proceed();
+    }
+*/
+
+    // 记录非登录操作日志
+    @Around("execution(* cn.bysjm.controller.*.*.*(..))")
+    public Object saveLog(ProceedingJoinPoint pjp) throws Throwable {
+        // 执行保存日志操作方法
+        this.saveLogPojo(pjp);
+        //执行原方法
+        return pjp.proceed();
+    }
+
+
+    // 记录登录 日志
+    @Around("execution(* cn.bysjm.controller.LoginController.login(..))")
+    public Object saveLogLogin(ProceedingJoinPoint pjp) throws Throwable {
+        // 执行原方法
+        Object object = pjp.proceed();
+        // 执行保存日志操作方法
+        this.saveLogPojo(pjp);
+
+        return object;
+    }
+
+    // 记录登出日志
+    @Around("execution(* cn.bysjm.controller.LoginController.logout(..))")
+    public Object saveLogLogout(ProceedingJoinPoint pjp) throws Throwable {
+        // 执行保存日志操作方法
+        this.saveLogPojo(pjp);
+        // 执行原方法
+        Object object = pjp.proceed();
+
+        return object;
+    }
+
+    // 保存日志操作
+    public void saveLogPojo(ProceedingJoinPoint pjp) {
+        SysLog sysLog = new SysLog();
+        User user = (User) session.getAttribute("loginUser");
+        if (user == null) {
+            return;
+        }
+        sysLog.setId(UUID.randomUUID().toString());
+        sysLog.setUserName(user.getUserName());
+        String ip = request.getRemoteAddr();
+        sysLog.setIp(ip);
+        sysLog.setTime(new Date());
+        sysLog.setCompanyId(user.getCompanyId());
+        sysLog.setCompanyName(user.getCompanyName());
+        MethodSignature signature = (MethodSignature) pjp.getSignature();
+        Method method = signature.getMethod();//从方法签名中获取方法对象
+        sysLog.setMethod(method.getName());//操作方法名
+
+        if (method.isAnnotationPresent(RequestMapping.class)){
+            sysLog.setAction(method.getAnnotation(RequestMapping.class).name());//方法上@RequestMapping注解的name值
+        }
+        sysLogService.save(sysLog);
     }
 
 }
